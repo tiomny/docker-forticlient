6e4d5bb9cc0739752f5a405eaa8f6fbeffcfca58
James <cadab1@gmail.com>
2016-10-25 13:20:31 +0100

    Added rdp forward

--- start.sh
+++ start.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
-if [ -z "$VPNADDR" -o -z "$VPNUSER" -o -z "$VPNPASS" ]; then
-  echo "Variables VPNADDR, VPNUSER and VPNPASS must be set."; exit;
+if [ -z "$VPNADDR" -o -z "$VPNUSER" -o -z "$VPNPASS" -o -z "$VPNRDPIP" ]; then
+  echo "Variables VPNRDPIP, VPNADDR, VPNUSER and VPNPASS must be set."; exit;
 fi
 
 export VPNTIMEOUT=${VPNTIMEOUT:-5}
+
+iptables -t nat -A PREROUTING -p tcp --dport 3380 -j DNAT --to-destination  ${VPNRDPIP}:3389
+iptables -t nat -A POSTROUTING -j MASQUERADE
 
 # Setup masquerade, to allow using the container as a gateway
 for iface in $(ip a | grep eth | grep inet | awk '{print $2}'); do
